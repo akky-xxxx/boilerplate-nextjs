9c0b76a20d89599f7680783f2e1230763882394c
akky--xxxx <114.s.aki@gmail.com>
2022-02-07 18:11:53 +0900

    chore(strapi): downgrade strapi to latest on version 3

---
+++ packages/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '59c15a50a792db52b7a6894b30062fe5'),
+    },
+  },
+});
---
+++ packages/strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88a4e812-858a-4a58-9ee5-a466a69b66f6'
+};
