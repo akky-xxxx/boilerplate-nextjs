13ade4726126cc159a3ea8172adc8aa949714669
akky--xxxx <114.s.aki@gmail.com>
2022-02-07 18:13:20 +0900

    chore(nextjs): add scripts for api client generator

---
+++ packages/nextjs/spec/openapi2aspida-config/strapi.js
@@ -0,0 +1,8 @@
+module.exports = {
+  input: "./src/libs/apiClient/strapi",
+  outputEachDir: false,
+  trailingSlash: false,
+  openapi: {
+    inputFile: "../strapi/extensions/documentation/documentation/1.0.0/full_documentation.json",
+  },
+}
